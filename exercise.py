5b4e21ae08487697e2a855dd66798390b271fd56
Ricardo Quintero <55771796+rquinteroTecSin@users.noreply.github.com>
2025-03-04 02:00:56 +0000

    Actualizar el nombre del modelo de IA en exercise.py de "gpt-4o-mini" a "gpt-4o"

--- exercise.py
+++ exercise.py
@@ -18,7 +18,7 @@ def obtener_pista_creativa(intentos, numero_secreto):
 
     token = os.environ["GITHUB_TOKEN"]
     endpoint = "https://models.inference.ai.azure.com"
-    model_name = "gpt-4o-mini"
+    model_name = "gpt-4o"
 
     client = OpenAI(
         base_url=endpoint,
